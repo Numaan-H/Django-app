fc838b1ac65ca2693c99213208608108575767a6
Numaan-H <rnahussain@gmail.com>
2025-11-11 15:30:08 +0000

    issues Reporting form

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
+
 
 app_name = 'itreporting'
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('contact', views.contact, name='contact'),
+    path('report/', PostListView.as_view(), name = 'report'),
+    path('issues/<int:pk>', PostDetailView.as_view(), name = 'issue-detail'),
 ]
