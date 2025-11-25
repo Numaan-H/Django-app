33b47b0b6782d3cb7fdc658e670a62e92337ac0d
Numaan-H <rnahussain@gmail.com>
2025-11-25 15:46:32 +0000

    deployed to azure cloud

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('issues/<int:pk>', PostDetailView.as_view(), name = 'issue-detail'),
     path('issue/new', PostCreateView.as_view(), name = 'issue-create'),
     path('issues/<int:pk>/update/', PostUpdateView.as_view(), name = 'issue-update'),
-    path('issue/<int:pk>/delete/', PostDeleteView.as_view(), name = 'issue-delete'),
+    path('issues/<int:pk>/delete/', PostDeleteView.as_view(), name = 'issue-delete'),
 ]
